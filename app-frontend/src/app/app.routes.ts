8c85016f438a6576d8bec43b9ce18ade8db445a3
Ayana <ayanabs2003@gmail.com>
2024-12-09 15:01:56 +0530

    homepage
    some formatting

--- app-frontend/src/app/app.routes.ts
+++ app-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SeatingComponent } from './seating/seating.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },    // Default route
+  { path: 'seating', component: SeatingComponent } // Route for seating
+];
